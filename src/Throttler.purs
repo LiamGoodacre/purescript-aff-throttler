d0ba2c65ef9704dffe7b3da72af1e610dd6faeaa
Liam Goodacre <goodacre.liam@gmail.com>
2018-12-08 13:17:41 +0000

    Init

---
+++ src/Throttler.purs
@@ -0,0 +1,102 @@
+module Throttler
+ ( Throttler
+ , new
+ , run
+ , runSimple
+ , kill
+ ) where
+
+import Prelude
+
+import Data.Foldable (traverse_)
+import Data.Maybe (Maybe(..))
+import Effect.Aff (Aff)
+import Effect.Aff as Aff
+import Effect.Aff.AVar (AVar)
+import Effect.Aff.AVar as AVar
+import Effect.Exception as Exception
+import Unsafe.Coerce (unsafeCoerce)
+
+
+type State =
+  { queuedFiber ∷ Maybe (Aff.Fiber Unit)
+  , currentFiber ∷ Maybe (Aff.Fiber Unit)
+  }
+
+initState ∷ State
+initState =
+  { queuedFiber: Nothing
+  , currentFiber: Nothing
+  }
+
+-- Throttler input = ∃ b c . ThrottlerF input b c
+type ThrottlerF input b c =
+  { setup ∷ Aff.Fiber input → Aff b
+  , operation ∷ Aff.Fiber b → Aff c
+  , teardown ∷ Aff.Fiber c → Aff Unit
+  , stateVar ∷ AVar State
+  }
+
+foreign import data Throttler ∷ Type → Type
+
+makeThrottler ∷ ∀ input b c . ThrottlerF input b c → Throttler input
+makeThrottler = unsafeCoerce
+
+withThrottler ∷ ∀ input r . (∀ b c . ThrottlerF input b c → r) → Throttler input → r
+withThrottler f t = f (unsafeCoerce t)
+
+new ∷
+  ∀ input b c .
+  (Aff.Fiber input → Aff b) →
+  (Aff.Fiber b → Aff c) →
+  (Aff.Fiber c → Aff Unit) →
+  Aff (Throttler input)
+new setup operation teardown =
+  AVar.new initState <#> \stateVar →
+    makeThrottler
+      { setup
+      , operation
+      , teardown
+      , stateVar
+      }
+
+kill ∷ ∀ input . Exception.Error → Throttler input → Aff Unit
+kill error = withThrottler \ { stateVar } →
+  AVar.kill error stateVar
+
+modifyAVar ∷ ∀ a . AVar a → (a → a) → Aff Unit
+modifyAVar avar f = AVar.take avar >>= \x → AVar.put (f x) avar
+
+run ∷ ∀ input . Throttler input → Aff input → Aff (Aff.Fiber Unit)
+run throttler input =
+  throttler # withThrottler \ { setup, operation, teardown, stateVar } → do
+    { queuedFiber, currentFiber } <- AVar.read stateVar
+    case queuedFiber of
+      Just queued → pure queued
+
+      Nothing → do
+        update <- Aff.suspendAff do
+          setupFiber <- Aff.suspendAff do
+             setup =<< Aff.forkAff input
+
+          Aff.sequential $
+            Aff.parallel (traverse_ Aff.joinFiber currentFiber) <*
+            Aff.parallel (Aff.attempt $ Aff.joinFiber setupFiber)
+
+          modifyAVar stateVar \state →
+            state
+              { queuedFiber = Nothing
+              , currentFiber = state.queuedFiber
+              }
+
+          operationFiber <- Aff.suspendAff $ operation setupFiber
+          outcome <- Aff.attempt $ Aff.joinFiber operationFiber
+          modifyAVar stateVar _ { currentFiber = Nothing }
+          teardown operationFiber
+
+        modifyAVar stateVar _ { queuedFiber = Just update }
+        Aff.forkAff $ Aff.joinFiber update
+
+runSimple ∷ Throttler Unit → Aff (Aff.Fiber Unit)
+runSimple t = run t (pure unit)
+
---
+++ test/Main.purs
@@ -0,0 +1,65 @@
+module Test.Main where
+
+import Prelude
+
+import Data.Unfoldable (replicateA)
+import Data.Foldable (traverse_)
+import Data.Time.Duration (Milliseconds(..))
+import Effect (Effect)
+import Effect.Aff (Aff)
+import Effect.Aff as Aff
+import Effect.Aff.AVar (AVar)
+import Effect.Aff.AVar as AVar
+import Effect.Class (liftEffect)
+import Effect.Console (log)
+import Effect.Exception as Exception
+import Throttler as Throttler
+
+getInc :: AVar Int -> Aff Int
+getInc var = do
+  id ← AVar.take var
+  AVar.put (id + 1) var
+  pure id
+
+main ∷ Effect Unit
+main = Aff.launchAff_ do
+  let
+    setup getId = do
+      id ← Aff.joinFiber getId
+      liftEffect (log $ "Wait " <> show id)
+      Aff.delay (Milliseconds 100.0)
+      pure id
+
+    operation getId = do
+      id ← Aff.joinFiber getId
+      liftEffect (log $ "Work " <> show id)
+      Aff.delay (Milliseconds 50.0)
+      pure id
+
+    teardown getId = do
+      id ← Aff.joinFiber getId
+      liftEffect (log $ "Done " <> show id)
+
+  throttledSubmit ← Throttler.new setup operation teardown
+
+  counterVar1 ← AVar.new 0
+  counterVar2 ← AVar.new 0
+
+  let launchSubmits ∷ Aff (Array (Aff.Fiber Unit))
+      launchSubmits = do
+        Aff.delay (Milliseconds 50.0)
+        replicateA 20000 do
+          attemptId <- getInc counterVar1
+          Throttler.run throttledSubmit do
+            actualId <- getInc counterVar2
+            pure { attemptId, actualId }
+
+  traverse_ Aff.joinFiber =<<
+    launchSubmits <> launchSubmits <> launchSubmits <> launchSubmits
+
+  Throttler.kill (Exception.error "Killed") throttledSubmit
+  AVar.kill (Exception.error "Killed") counterVar1
+  AVar.kill (Exception.error "Killed") counterVar2
+
+  liftEffect $ log "End"
+
